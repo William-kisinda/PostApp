3bad4183af4b95e0a8eceda2e8e20f1f79c511ef
William-kisinda <williamkisinda@gmail.com>
2023-08-20 14:53:12 +0300

    Auth screen implementation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>PostApp</title>
+    <title>User Authentication</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        button {
+            padding: 10px;
+            background-color: #007bff;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
-<body class=" bg-gray-200">
-    <nav class=" p-6 bg-white flex justify-between mb-6">
-        <ul class="flex items-center">
-            <li><a href="/home" class="p-3">Home</a></li>
-            <li><a href="/dashboard" class="p-3">Dashboard</a></li>
-            <li><a href="/post" class="p-3">Post</a></li>
-        </ul>
-        <ul class="flex">
-            <li>William Kisinda</li>
-            <li><a href="/dashboard" class="p-3">Logout</a></li>
-        </ul>
-    </nav>
-    @yield('content')
+
+<body 
+<div class="container">
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</div>
+
+<div class="container">
+    <h2>Register</h2>
+    <form action="register.php" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="password" name="confirm_password" placeholder="Confirm Password" required>
+        <button type="submit">Register</button>
+    </form>
+</div>
+</body>
 </body>
 </html>
